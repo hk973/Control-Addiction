8afaa5858b0a3b93b5774a01f419e048388d653c
GenZopia <genzopia@gmail.com>
2025-05-07 16:08:43 +0530

    added id to all button

--- app/src/main/java/com/genzopia/addiction/SelectedAppsFragment.java
+++ app/src/main/java/com/genzopia/addiction/SelectedAppsFragment.java
@@ -77,8 +77,8 @@ public class SelectedAppsFragment extends Fragment implements OnBack{
         searchBar = view.findViewById(R.id.searchBar);
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-        menu a=new menu(view);
-        a.initializeview();
+        menu a=new menu(view,getContext());
+
         packageManager = requireActivity().getPackageManager();
         sharedPrefHelper = new SharedPrefHelper(requireContext());
         selectedApps = sharedPrefHelper.getSelectedAppValue();
--- app/src/main/java/com/genzopia/addiction/menu.java
+++ app/src/main/java/com/genzopia/addiction/menu.java
@@ -1,12 +1,15 @@
 package com.genzopia.addiction;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class menu {
-    Button bb;
-    public menu(View view) {
+   ImageView internet;
+    public menu(View view, Context context) {
 
 
     }
