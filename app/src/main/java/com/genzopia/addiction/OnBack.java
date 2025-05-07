a81695aa49b5f3d51c2346dd2fef1f47a990a7a4
GenZopia <genzopia@gmail.com>
2025-05-07 11:14:12 +0530

    notification pannel frontend

--- app/src/main/java/com/genzopia/addiction/OnBack.java
+++ app/src/main/java/com/genzopia/addiction/OnBack.java
@@ -1,10 +1,7 @@
 package com.genzopia.addiction;
 
-
 import androidx.viewpager2.widget.ViewPager2;
 
 public interface OnBack {
-
-
     void back(ViewPager2 viewPager);
 }
--- app/src/main/java/com/genzopia/addiction/menu.java
+++ app/src/main/java/com/genzopia/addiction/menu.java
@@ -7,16 +7,11 @@ import android.widget.Button;
 public class menu {
     Button bb;
     public menu(View view) {
-        bb=view.findViewById(R.id.btn_option1);
+
 
     }
 
     public void initializeview() {
-        bb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.e("test234","pressed");
-            }
-        });
+
     }
 }
