0f0a16e7f7b60b2df9dfd9717071a0c9cb823780
GenZopia <genzopia@gmail.com>
2025-07-02 12:50:11 +0530

    dd

--- app/src/main/java/com/genzopia/addiction/Alarm/Alarm_main_activity.kt
+++ app/src/main/java/com/genzopia/addiction/Alarm/Alarm_main_activity.kt
@@ -1,13 +1,19 @@
 package com.genzopia.addiction.Alarm
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,26 +23,10 @@ class Alarm_main_activity: ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(20.dp)
 
-            )
         }
 
     }
 
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.fillMaxSize()
-        )
-    }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-          Greeting("hariom")
-    }
 }
